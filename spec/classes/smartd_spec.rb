d4ab6d681a0e94e86de919cebdbe410d1a8b5562
Joshua Hoblitt <jhoblitt@cpan.org>
2013-06-06 17:06:38 -0700

    fix setting megaraid device options via device_opts => { 'megaraid' => ... }

--- spec/classes/smartd_spec.rb
+++ spec/classes/smartd_spec.rb
@@ -44,4 +44,36 @@ describe 'smartd' do
         END
     end
   end
+
+  context 'without params + megaraid facts' do
+    let(:facts) do
+      {
+        :osfamily=> 'RedHat', :lsbmajordistrelease => 6,
+        :megaraid_adapters        => '1',
+        :megaraid_virtual_drives  => '/dev/sdb,/dev/sda',
+        :megaraid_physical_drives => '2,1',
+      }
+    end
+
+    let(:params) do
+      {
+        :device_opts => { 'megaraid' => '-I 194'},
+      }
+    end
+
+    it do
+      should include_class('smartd')
+      should include_class('smartd::params')
+      should contain_package('smartmontools')
+      should contain_service('smartd')
+      should contain_file('/etc/smartd.conf')\
+        .with_content(<<-END.gsub(/^\s+/, ""))
+          # Managed by Puppet -- do not edit!
+          DEFAULT -m root -M daily
+          /dev/sda -d sat+megaraid,1 -I 194
+          /dev/sda -d sat+megaraid,2 -I 194
+          DEVICESCAN
+        END
+    end
+  end
 end
