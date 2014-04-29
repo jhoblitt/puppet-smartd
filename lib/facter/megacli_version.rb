f5751893b72519e8ef21cfcfa69e6696d36ccbff
Joshua Hoblitt <jhoblitt@cpan.org>
2014-04-29 11:13:51 -0700

    add megacli_version fact

---
+++ lib/facter/megacli_version.rb
@@ -0,0 +1,13 @@
+Facter.add(:megacli_version) do
+  megacli = Facter.value(:megacli)
+
+  setcode do
+    unless megacli.nil?
+      output = Facter::Util::Resolution.exec("#{megacli} -Version -Cli -aALL")
+      next if output.nil?
+      m = output.match(/MegaCLI SAS RAID Management Tool  Ver ([\d\.]+)/)
+      next unless m.size == 2
+      m[1]
+    end
+  end
+end
