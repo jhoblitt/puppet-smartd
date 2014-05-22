ebf88cdb9096f4c1d5cbf134b79ef7664a482008
Joshua Hoblitt <jhoblitt@cpan.org>
2014-05-21 17:41:21 -0700

    update megaraid_serial fact to deal with SM motherboards
    Per #29, the LSI controller on at least some models of SuperMicro
    motherboards does not report a serial number.  The megaraid_serial fact
    has not gracefully handling this case and facter was generating errors.

--- lib/facter/megaraid_serial.rb
+++ lib/facter/megaraid_serial.rb
@@ -6,6 +6,7 @@ Facter.add(:megaraid_serial) do
       output = Facter::Util::Resolution.exec("#{megacli} -Version -Ctrl -aALL -NoLog")
       next if output.nil?
       m = output.match(/Serial No : (\S+)\s*$/)
+      next if m.nil?
       next unless m.size == 2
       m[1]
     end
