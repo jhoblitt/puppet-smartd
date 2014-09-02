bbcae381fcb404d2d7b8ca551645cb9b57893711
Michael Arnold <github@razorsedge.org>
2014-09-01 17:26:53 -0700

    Better support for RedHat smartmontools 6.x.
    Fixing Fedora support similar to #36.

--- manifests/params.pp
+++ manifests/params.pp
@@ -36,7 +36,11 @@ class smartd::params {
     }
     'RedHat': {
       $config_file = $::operatingsystem ? {
-        'Fedora' => '/etc/smartd.conf',
+        'Fedora' => $::operatingsystemrelease ? {
+          # No, I am not going to support versions 1-9.
+          /10|11|12|13|14|15|16|17|18/ => '/etc/smartd.conf',
+          default                      => '/etc/smartmontools/smartd.conf',
+        },
         default  => $::operatingsystemmajrelease ? {
           '7'     => '/etc/smartmontools/smartd.conf',
           default => '/etc/smartd.conf',
