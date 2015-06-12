8b633d9bccacb7aff7d415580fef3b4eb8d16406
Simon Aquino <simon.aquino@digital.cabinet-office.gov.uk>
2015-06-12 19:01:07 +0100

    Fix wrong parameter name in params.pp
    It should be $manage_service not $service_manage!

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,7 +15,7 @@
 class smartd::params {
   $package_name       = 'smartmontools'
   $service_ensure     = 'running'
-  $service_manage     = true
+  $manage_service     = true
   $devicescan         = true
   $devicescan_options = undef
   $devices            = []
